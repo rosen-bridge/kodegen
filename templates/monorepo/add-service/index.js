8722f4ceabd5bdee320debfa3d71d674517dffd5
Mohammad Kermani <mkermani144@gmail.com>
2023-09-16 19:52:55 +0330

    chore: add missing jsdoc in `add-service` main script

--- templates/monorepo/add-service/index.js
+++ templates/monorepo/add-service/index.js
@@ -4,6 +4,10 @@ const prompt = require('./prompt');
 
 const PACKAGE_JSON = 'package.json';
 
+/**
+ * log an error and exits process because current working directory is not a
+ * monorepo root path
+ */
 const exitProcessDueToAbsentMonorepo = () => {
   console.error(
     'You should run this command in a monorepo root. You can create one by calling `npx kodegen monorepo init`.'
@@ -11,6 +15,10 @@ const exitProcessDueToAbsentMonorepo = () => {
   process.exit(1);
 };
 
+/**
+ * get package json object of current working directory or `null` if no package
+ * file is found
+ */
 const getCWDPackageJson = () => {
   if (!fs.existsSync(PACKAGE_JSON)) {
     return null;

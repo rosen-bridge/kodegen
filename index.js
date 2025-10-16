3187d1cb52a84ab6bf7ef2ce4423c0c2925c9013
Rasoul salehi <8raha6094@gmail.com>
2025-10-16 13:53:50 +0330

    help command was updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { runner } = require('hygen');
+const { runner , printHelp } = require('hygen');
 const Logger = require('hygen/dist/logger');
 const fs = require('fs');
 const path = require('path');
@@ -13,33 +13,21 @@ if (['--version', '-v'].includes(process.argv[2])) {
 }
 
 if (process.argv.includes('--help') || process.argv.includes('-h')) {
-  console.log('\nkodegen help\n');
-  const generatorsDir = path.join(__dirname, 'templates');
-    const generators = fs.readdirSync(generatorsDir);
-    if (generators.length === 0) {
-      console.log('No generators found in templates/');
-    } else {
-      generators.forEach(gen => {
-        const actionsPath = path.join(generatorsDir, gen);
-        if (fs.statSync(actionsPath).isDirectory()) {
-          const actions = fs.readdirSync(actionsPath);
-          console.log(`\n${gen}:`);
-          actions.forEach(a => {
-            const actionPath = path.join(actionsPath, a);
-            if (fs.statSync(actionPath).isDirectory()) {
-              console.log(`  - ${gen} ${a}`);
-            }
-          });
-        }
-      });
-    }
-    console.log('\nExamples:\n');
-    console.log('  npx kodegen monorepo init\n');
-    console.log('  in monorepo root:\n        - npx kodegen monorepo add-package  \n        - npx kodegen monorepo add-service');
-    console.log('\n  npx kodegen repo init');
-    console.log('\nFor more information, visit Documentation: https://github.com/rosen-bridge/kodegen\n'); 
-    process.exit(0);
-  }
+  printHelp(templates = defaultTemplates, new Logger.default(console.log.bind(console)));
+      console.log(`\nExamples:
+    npx kodegen monorepo init
+
+    In monorepo root:
+      - npx kodegen monorepo add-package
+      - npx kodegen monorepo add-service
+
+    npx kodegen repo init
+
+  For more information, visit:
+    https://github.com/rosen-bridge/kodegen
+`);
+  process.exit(0);
+}
 
 runner(process.argv.slice(2), {
   templates: defaultTemplates,
